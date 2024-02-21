09f214a8aa3bce8bbfae478e162308c8efad28e5
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-21 22:36:23 +0700

    Update Home.jsx

--- my-react-app/src/pages/Home/Home.jsx
+++ my-react-app/src/pages/Home/Home.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <div class="header-with-seach">
+                <div class="header_logo">
+                    <img src='../img-eFruit./img-logo.png' alt='logo'></img>
+                </div>
+            </div>
+        </div>
     )
 }
 
