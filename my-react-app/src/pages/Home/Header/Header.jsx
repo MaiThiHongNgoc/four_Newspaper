83da17e3c5006cdee2026658775af9f64847987d
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-23 00:53:32 +0700

    .

--- my-react-app/src/pages/Home/Header/Header.jsx
+++ my-react-app/src/pages/Home/Header/Header.jsx
@@ -14,11 +14,11 @@ const Header = () => {
                 <img src="https://desero-store-demo.myshopify.com/cdn/shop/files/logo.png?v=1657783401" alt="" />
             </div>
             <div className="navbar-header">
-                <div className="navbar-header-home"> <Link to="/">Home</Link></div>
-                <div className="navbar-header-menu"> <Link to="/Menu">Menu</Link></div>
-                <div className="navbar-header-shop"> <Link to="/Shop">Shop</Link></div>
-                <div className="navbar-header-pages"> <Link to="/Pages">Pages</Link></div>
-                <div className="navbar-header-blogs"> <Link to="/Blogs">Blogs</Link></div>
+                <div className="navbar-header-home"> <Link to="/">HOME</Link></div>
+                <div className="navbar-header-menu"> <Link to="/Menu">MENU</Link></div>
+                <div className="navbar-header-shop"> <Link to="/Shop">SHOP</Link></div>
+                <div className="navbar-header-pages"> <Link to="/Pages">PAGES</Link></div>
+                <div className="navbar-header-blogs"> <Link to="/Blogs">BLOGS</Link></div>
 
             </div>
             <div className="components-header">
