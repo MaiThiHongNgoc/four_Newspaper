5ab1eafa0c2fde52eefc96e73e5b63bf35af7301
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-23 11:05:18 +0700

    .

--- my-react-app/src/pages/Home/Home.jsx
+++ my-react-app/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header/Header'
-import Conten from './Conten/Conten'
+import Conten from './Content/Conten'
 //import { Slide } from 'react-slideshow-image'
 const Home = () => {
     return (
