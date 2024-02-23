285dc395a07cd9b96a4cab9f873b8270f5756f5b
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-23 11:55:36 +0700

    .

--- my-react-app/src/pages/Home/Content/Slide/Slide.jsx
+++ my-react-app/src/pages/Home/Content/Slide/Slide.jsx
@@ -5,9 +5,9 @@ import './Slide.scss'
 
 const Slideshow = () => {
     const images = [
-        "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-        "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-        "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+        "https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.1.png?v=1657868808&fbclid=IwAR0bmGd3OHSjTSORPnzZ5s3U94QgAFnaMrnU6tvcY9qOeR2bjlNfsBpuRjk",
+        "https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.2.png?v=1657868808&fbclid=IwAR2DsNcwcTQmZJlVe3d3kORLgK2jiMGqQrIC-OOC5UJuHtHf3HR5ORWE-4M",
+        "https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.2.png?v=1657868808&fbclid=IwAR2DsNcwcTQmZJlVe3d3kORLgK2jiMGqQrIC-OOC5UJuHtHf3HR5ORWE-4M",
     ];
 
     return (
