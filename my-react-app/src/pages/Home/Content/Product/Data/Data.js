b52473eb7cf31bb11836b8ddf7ef5fa364751114
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-24 02:30:46 +0700

    Create Data.js

---
+++ my-react-app/src/pages/Home/Content/Product/Data/Data.js
@@ -0,0 +1,56 @@
+export const dataProduct = [
+    {
+    id:1,
+   img: "https://desero-store-demo.myshopify.com/cdn/shop/products/13.4.png?v=1656902509", 
+   title: "Synthetic Smoothies",
+   price :60.00
+   },
+    {
+   id:2,
+   img: "https://desero-store-demo.myshopify.com/cdn/shop/products/9.1.png?v=1656908805",
+   title: "Strawberry Smoothie",
+   price :55.00
+   },
+   {
+    id:3 ,
+    img: "https://desero-store-demo.myshopify.com/cdn/shop/products/11.1.png?v=1656903842",
+    title: "Raspberry Smoothie",
+    price :54.00
+    },
+    {
+    id:4 ,
+    img: "https://desero-store-demo.myshopify.com/cdn/shop/products/12.1.png?v=1656903730",
+    title: "Peach Smoothie",
+    price :56.00
+    },
+    {
+    id:5 ,
+    img: "https://desero-store-demo.myshopify.com/cdn/shop/products/10.3.png?v=1656903572",
+    title: "Mango Smoothie",
+    price :50.00
+    },
+    {
+     id:6 ,
+    img: "https://desero-store-demo.myshopify.com/cdn/shop/products/15.1.png?v=1656902928",
+    title: "Blueberry Smoothie",
+    price :58.00
+    },
+    {
+    id:7 ,
+    img: "https://desero-store-demo.myshopify.com/cdn/shop/products/8.1.png?v=1656902846",
+    title: "Banana Smoothie",
+    price :50.00
+    },
+    {
+    id:8 ,
+    img: "https://desero-store-demo.myshopify.com/cdn/shop/products/14.1.png?v=1656902680",
+    title: "Apple Smoothie",
+    price :50.00
+    },
+
+
+    
+
+   
+   
+   ]
