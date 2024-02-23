e7601a41f479b84af2aac2b6687899e79e16d068
trungle0806 <trungltrh@gmail.com>
2024-02-24 00:23:00 +0700

    .

--- my-react-app/src/pages/Home/Content/Banner/Banner.jsx
+++ my-react-app/src/pages/Home/Content/Banner/Banner.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+   <div id="shopify-section-template--16186641678559__165707416589a3879f" class="shopify-section">
+    <div data-section-id="template--16186641678559__165707416589a3879f" data-section-type="section-banner-v5" style="">
+      <div class="section-banner-v5 mt-all">
+        <div class="container container-v1">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="banner-item">
+                <div class="banner-img">
+                  <a href="/collections/all" class="box-img">
+                    <img data-src=" //desero-store-demo.myshopify.com/cdn/shopfiles/bn1.3.png?v=1657868808 " class="img-fluid w-100 ls-is-cached lazyloaded" alt="_banner_img_" src="//desero-story-demo.myshopify.com/cdn/shop/files/bn1.3.png?v=1657868808">  </img>                
+                  </a>
+                </div>
+                <div class="banner-info">
+                  <p>=SALE UP TO 30%</p>
+                  <h3 class="title-banner" >orange juice</h3>
+                  <a href>= SHOP NOW</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
 
