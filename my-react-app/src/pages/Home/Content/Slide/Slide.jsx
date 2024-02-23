087aacbd645e751c9c388ec344e412889e59c153
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-23 12:08:36 +0700

    Update Slide.jsx

--- my-react-app/src/pages/Home/Content/Slide/Slide.jsx
+++ my-react-app/src/pages/Home/Content/Slide/Slide.jsx
@@ -7,7 +7,7 @@ const Slideshow = () => {
     const images = [
         "https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.1.png?v=1657868808&fbclid=IwAR0bmGd3OHSjTSORPnzZ5s3U94QgAFnaMrnU6tvcY9qOeR2bjlNfsBpuRjk",
         "https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.2.png?v=1657868808&fbclid=IwAR2DsNcwcTQmZJlVe3d3kORLgK2jiMGqQrIC-OOC5UJuHtHf3HR5ORWE-4M",
-        "https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.2.png?v=1657868808&fbclid=IwAR2DsNcwcTQmZJlVe3d3kORLgK2jiMGqQrIC-OOC5UJuHtHf3HR5ORWE-4M",
+        "https://desero-store-demo.myshopify.com/cdn/shop/files/bn2.1.png?v=1657869067&fbclid=IwAR0oRIYOKUetSdPVFv5APRwSPo2xkBaTSubT-H7cqqA_EUdImAZTkZUQj7I",
     ];
 
     return (
