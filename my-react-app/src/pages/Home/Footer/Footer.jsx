c8944e6d464167779f1035e3cfa117d89d5e42ea
QuachDangDiep <145833428+QuachDangDiep@users.noreply.github.com>
2024-02-24 23:28:22 +0700

    .

--- my-react-app/src/pages/Home/Footer/Footer.jsx
+++ my-react-app/src/pages/Home/Footer/Footer.jsx
@@ -1,162 +1,142 @@
 import React from 'react';
 import 'react-icons'
+import './Footer.scss' ;
 import { FaPaperPlane } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaDribbble } from "react-icons/fa";
 import { FaBehance } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
-  return (
-       <div>
+  return ( 
         <footer className='footer_v2'>
           <div className="top-footer">
-            <div className="container container-v1">
-              <div className="row">
-                <div className="col-lg-3 col-md-12">
-                  <div className="info_footer end">
                     <div className="logo-top">
-                      <a href="https://desero-store-demo.myshopify.com/cdn/shop/files/logo.png?v=1657783401" className="logo">
-                        <img src="//desero-store-demo.myshopify.com/cdn/shop/files/logo.png?v=1657783401" alt="Desero - Organic Fruit & Juice Responsive Shopify Theme" className="img-fluid" />
+                      <a href="" className="logo">
+                        <img src="https://desero-store-demo.myshopify.com/cdn/shop/files/logo.png?v=1657783401" alt="Desero - Organic Fruit & Juice Responsive Shopify Theme" className="img-fluid" />
                       </a>
                     </div>
-                    <p className="mb-0 content_footer">
-                      Subscribe our newsletter and get
-                    </p>
+                    <p className="conten">Subscribe our newsletter and get</p>
                     <br></br>
-                    <p className="mb-0 content_footer">discount 30% off</p>
+                    <p className= "content_footer">discount 30% off</p>
+
                     <div className="newletter_email">
-                      <form action="#" method="post" className="needs-validation form-inline">
                         <div className="form-group">
                           <input type="email" name="EMAIL" className="form-control" placeholder="Your email address..." required/>
                         </div>
                         <button className="btn" type="submit">
                         <FaPaperPlane />
                         </button>
-                      </form>
                     </div>
+
                     <div className="list-icon">
-                      <ul className="list-inline list-unstyled mb-0">
+                      <ul className="list-unstyled">
                         <li className="list-inline-item">
-                          <a href title target="_blank" className="social-item">
+                          <a  className="social-item">
                           <i className="twitter"><FaTwitter /></i>
                           </a>
                         </li>
                         <li className="list-inline-item">
-                          <a href title target="_blank" className="social-item">
+                          <a className="social-item">
                           <i className="dribbble"><FaDribbble /></i>
                           </a>
                         </li>
                         <li className="list-inline-item">
-                          <a href title target="_blank" className="social-item">
+                          <a  className="social-item">
                           <i className="dribbble"><FaBehance /></i>
                           </a>
                         </li>
                         <li className="list-inline-item">
-                          <a href title target="_blank" className="social-item">
-                          <i className="dribbble"><FaInstagram /></i>
+                          <a  className="social-item">
+                          <i className="instagram"><FaInstagram /></i>
                           </a>
                         </li>      
                       </ul>
                     </div>
-                  </div>
-                </div>
-                <div className="col-lg-3 col-md-4 border-ft">
+    
                   <div className="info_footer">
                     <div className="title_footer">
-                      <h4 className="mb-0 title_border">Customer Case</h4>
+                      <h4>Customer Case</h4>
                     </div>
-                    <ul className="list-unstyled mb-0">
+                    <ul className="list-unstyled">
                       <li>
-                        <a href="#" title="delivery ">Delivery</a>
+                        <a href="">Delivery</a>
                       </li>
                       <li>
-                        <a href="#" title="legal notice ">Legal Notice</a>
+                        <a href="">Legal Notice</a>
                       </li>
                       <li>
-                        <a href="#" title="documentation ">Documentation</a>
+                        <a href="">Documentation</a>
                       </li>
                       <li>
-                        <a href="#" title="secure payment ">Secure payment</a>
+                        <a href="">Secure payment</a>
                       </li>
                       <li>
-                        <a href="#" title="stores ">Stores</a>
+                        <a href="">Stores</a>
                       </li>
                     </ul>
                   </div>
-                </div>
-                <div className="col-lg-3 col-md-4 border-ft">
+                
                   <div className="info_footer">
                     <div className="title_footer">
-                      <h4 className="mb-0 title_border">Quick Shop</h4>
+                      <h4>Quick Shop</h4>
                     </div>
-                    <ul className="list-unstyled mb-0">
+                    <ul className="list-unstyled">
                     <li>
-                        <a href="#" title="pagination ">Delivery</a>
+                        <a href="">Pagination</a>
                       </li>
                       <li>
-                        <a href="#" title="terms & condition ">Terms & Condition</a>
+                        <a href="">Terms & Condition</a>
                       </li>
                       <li>
-                        <a href="#" title="contact ">Contact</a>
+                        <a href="">Contact</a>
                       </li>
                       <li>
-                        <a href="#" title="home page ">Home page</a>
+                        <a href="">Home page</a>
                       </li>
                       <li>
-                        <a href="#" title="term to us ">Term of us</a>
+                        <a href="">Term of us</a>
                       </li>
                     </ul>
                   </div>
-                </div>
-                <div className="col-lg-3 col-md-4 border-ft">
+                
                   <div className="info_footer">
                     <div className="title_footer">
-                      <h4 className="mb-0 title_border">Company</h4>
+                      <h4>Company</h4>
                     </div>
-                    <ul className="list-unstyled mb-0">
+                    <ul className="list-unstyled">
                     <li>
-                        <a href="#" title="help center ">Help Center</a>
+                        <a href="">Help Center</a>
                       </li>
                       <li>
-                        <a href="#" title="address store ">Address Store</a>
+                        <a href="">Address Store</a>
                       </li>
                       <li>
-                        <a href="#" title="privacy policy ">Privacy Policy</a>
+                        <a href="">Privacy Policy</a>
                       </li>
                       <li>
-                        <a href="#" title="receivers & amplifiers ">Receivers & Amplifiers</a>
+                        <a href="">Receivers & Amplifiers</a>
                       </li>
                       <li>
-                        <a href="#" title="clothings ">Clothings</a>
+                        <a href="">Clothings</a>
                       </li>
                     </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
+                  </div>  
           </div>
           <div className="copyright">
-            <div className="container container-v1">
-              <div className="row align-items-center">
-                <div className="col-lg-6 col-md-6 text-lg-left text-md-left">
-                  <p className="text-copyright mb-0"></p>
-                  <div className="text-copyright mb-0">
-                    " © Copyright 2022 | "
-                    <a href="https://shopilaunch.com/" className="underline_hover bold">Desero</a>
+            <div className="container">
+                  <div className="text-copyright">
+                     © Copyright 2022 | 
+                    <a href="https://shopilaunch.com/" className="underline_hover-bold">Desero</a>
                     <p>By</p>
-                    <a href="https://shopilaunch.com/" className="underline_hover link">ShopiLaunch.</a>
-                    <a href="https://www.shopify.com/?ref=shopifycenter" className="underline_hover link">Powered by Shoppify.</a>
+                    <a href="https://shopilaunch.com/" className="underline_hover-link">ShopiLaunch.</a>
+                    <a href="https://www.shopify.com/?ref=shopifycenter" className="underline_hover-link">Powered by Shoppify.</a>
                   </div>
-                  <p></p>
-                </div>
-                <div className="col-lg-6 col-md-6 text-lg-right text-md-right ">
+                <div className="cart">
                   <img src="https://desero-store-demo.myshopify.com/cdn/shop/files/payment.png?v=1656983820" alt="" />
                 </div>
-              </div>
             </div>
           </div>
-        </footer>
-       </div>
+        </footer>    
   )
 }
 
