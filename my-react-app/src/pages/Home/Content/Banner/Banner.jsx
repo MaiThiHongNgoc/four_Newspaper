4f3d7dac024555f606a73095bef5f7dfcd35f3d9
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-25 14:54:00 +0700

    banner

--- my-react-app/src/pages/Home/Content/Banner/Banner.jsx
+++ my-react-app/src/pages/Home/Content/Banner/Banner.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import './Banner.scss'
-const Banner = () => {
+import React,{useState }from 'react';
+import './Banner.scss';
+const Banner = ({src, alt, zoomScale}) => {
+  const [zoom, setZoom] = useState(false);
+
+  //hàm sử lý khi di chuột vào
+  const handleMouseEnter = () => {
+    setZoom(true);
+  };
+
+  //hàm sử lý khi di chuột ra
+  const handleMouseLeave =() =>{
+    setZoom(false);
+  };
   return (
            <div className="banner">
               <div className="banner-item">
-                <div className="banner-img">
-                    <img src="https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.3.png?v=1657868808" alt="" /> 
+                <div className={`banner-img ${zoom ? 'zoomed' : ''}`}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <img src={"https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.3.png?v=1657868808"}  alt={alt || 'Zoomable Image'}/> 
                     <div className="banner-info">
                       <p>SALE UP TO 30%</p>
                       <h3 >Kiwi Juice</h3>
@@ -15,8 +29,11 @@ const Banner = () => {
               </div>
 
               <div className="banner-item">
-                <div className="banner-img">
-                    <img src="https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.4.png?v=1657868808" alt="" /> 
+              <div className={`banner-img ${zoom ? 'zoomed' : ''}`}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <img src={"https://desero-store-demo.myshopify.com/cdn/shop/files/bn1.4.png?v=1657868808"}  alt={alt || 'Zoomable Image'}/> 
                     <div className="banner-info">
                      <p>SALE UP TO 30%</p>
                      <h3 >Orange Juice</h3>
@@ -26,9 +43,8 @@ const Banner = () => {
               </div>
                 
             </div>
-          
-              
-             
+
+    
   )
 }
 
