7cb98ceb4491d683062db1d08c821095b4c988eb
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-02-27 13:56:02 +0700

    .

--- my-react-app/src/pages/Home/Content/Conten.jsx
+++ my-react-app/src/pages/Home/Content/Conten.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Slide from './Slide/Slide'
 import Banner from './Banner/Banner'
 import Product from './Product/Product'
+import Product2 from './Product-2/Product2'
 import OurBlogs from './OurBlogs/OurBlogs'
 import BestSeller from './Product/BestSellers/BestSeller'
 import TopRate from './Product/TopRates/TopRate'
-import Product2 from './Product-2/Product2'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const Conten = () => {
   return (
@@ -13,15 +13,10 @@ const Conten = () => {
         <Slide/>
         <Banner/>
         <Product/>
-      
-      <Routes>
-        
-        <Route path="/best-sellers" element={<BestSeller />} />
-        <Route path="/top-rates" element={<TopRate />} />
-        
-      </Routes>
-    
-        <OurBlogs/>
+              <Routes>
+                  <Route path="/best-sellers" element={<BestSeller />} />
+                  <Route path="/top-rates" element={<TopRate />} />
+               </Routes>
         <Product2/>
 
     </div>
--- my-react-app/src/pages/Home/Home.jsx
+++ my-react-app/src/pages/Home/Home.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header/Header'
 import Conten from './Content/Conten'
-// import Footer from './Footer/Footer'
+import Footer from './Footer/Footer'
 const Home = () => {
     return (
         <div>
             <Header />
             <Conten/>
-            {/* <Footer/> */}
+            <Footer/>
         </div>
     )
 }
