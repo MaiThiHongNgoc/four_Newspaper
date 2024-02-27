833e432ab58bcabe141e5986f205cd7e8dba44d0
QuachDangDiep <145833428+QuachDangDiep@users.noreply.github.com>
2024-02-27 13:39:45 +0700

    .

---
+++ my-react-app/src/pages/Home/Content/Product-2/Product2.jsx
@@ -0,0 +1,228 @@
+import React from "react";
+import "./Product2.scss";
+import { CiStar } from "react-icons/ci";
+const Product2 = () => {
+    return (
+        <div className="container">
+
+            <div className="content">
+                <div className="banner-ver">
+                    <div className="product">
+                        <h3>Featured Products</h3>
+                    </div>
+                </div>
+
+                <div className="listproduct">
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/9.1.png?v=1656908805" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Strawberry Smoothie</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$55.00</span>
+                        </p>
+                    </div>
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/12.1.png?v=1656903730" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Peach Smoothie</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$56.00</span>
+                        </p>
+                    </div>
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/10.1.png?v=1656903572" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Mango Smoothie</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$50.00</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="content">
+                <div className="banner-ver">
+                    <div className="product">
+                        <h3>Onsale Products</h3>
+                    </div>
+                </div>
+
+                <div className="listproduct">
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/5.1_327828dd-b376-4d0e-9d43-3cba0d9e5ad6.png?v=1656903648" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Orange Juice</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$55.00</span>
+                        </p>
+                    </div>
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/2.1.png?v=1656903318" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Kiwi Juice</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$45.00</span>
+                        </p>
+                    </div>
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/16.1.png?v=1656903198" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Guava Juice</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$40.00</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="content">
+                <div className="banner-ver">
+                    <div className="product">
+                        <h3>Onsale Products</h3>
+                    </div>
+                </div>
+
+                <div className="listproduct">
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/13.1.png?v=1656902509" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Synthetic Smoothies</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$60.00</span>
+                        </p>
+                    </div>
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/9.1.png?v=1656908805" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Strawberry Smoothie</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$55.00</span>
+                        </p>
+                    </div>
+
+                    <div className="product_item">
+                        <div className="img-product">
+                            <img src="https://desero-store-demo.myshopify.com/cdn/shop/products/11.1.png?v=1656903842" alt="" />
+                        </div>
+                    </div>
+                    <div className="info-product">
+                        <h4 >Raspberry Smoothie</h4>
+                        <span className="star">
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                            <i className="icon"><CiStar /></i>
+                        </span>
+                        <span className="caption">No reviews</span>
+                        <p>
+                            <span className="price">$54.00</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Product2;
