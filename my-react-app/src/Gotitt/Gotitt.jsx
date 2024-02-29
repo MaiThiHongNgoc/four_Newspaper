a0499666c9bf12a7a042541426d02b2403eeb2e3
trungle0806 <trungltrh@gmail.com>
2024-02-29 16:38:25 +0700

    .

---
+++ my-react-app/src/Gotitt/Gotitt.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Gotitt.scss"
+const Gotitt = () => {
+  return (
+    <div className="Gotitt">
+        <div className="nocite-text">
+            <p className="text">This website uses cookies to ensure you get the best experience on our website.</p>
+        </div>
+        <a className="polici">Privacy & Policy</a>
+        <a className="close-cookie">Got it!</a> 
+    </div>
+  )
+}
+
+export default Gotitt
