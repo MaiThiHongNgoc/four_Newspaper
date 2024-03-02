3aebb320cc024549e0da6a965aa044b88e383b75
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2024-03-02 15:06:14 +0700

    sécion

--- my-react-app/src/pages/Home/Content/Conten.jsx
+++ my-react-app/src/pages/Home/Content/Conten.jsx
@@ -4,6 +4,7 @@ import Banner from './Banner/Banner'
 import Product from './Product/Product'
 import OurBlogs from './OurBlogs/OurBlogs'
 import Product2 from './Product-2/Product2'
+//import Section from '../../../Section/Section'
 // import Register from '../../Register/Register'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const Conten = () => {
@@ -14,6 +15,7 @@ const Conten = () => {
         <Product/>
              
                <OurBlogs/>
+               {/* <Section/> */}
         <Product2/>
         {/* <Register/> */}
     </div>
