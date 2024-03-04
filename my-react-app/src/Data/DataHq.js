a39b2f487d3b31f336cc8792bfb85da101565571
QuachDangDiep <145833428+QuachDangDiep@users.noreply.github.com>
2024-03-04 23:32:29 +0700

    .

---
+++ my-react-app/src/Data/DataHq.js
@@ -0,0 +1,88 @@
+export const dataProduct = [
+    // Nước ép hoa quả
+    {
+   id:1,
+   img: "https://blog.ptlvina.com/wp-content/uploads/2021/02/nuoc-ep-cam-6.jpg", 
+   title: "Orange juice",
+   price :65.00
+   },
+   {
+   id:2,
+   img: "https://bazaarvietnam.vn/wp-content/uploads/2022/03/harper-bazaar-cach-lam-nuoc-ep-dua-hau-2-e1647506440642.jpeg", 
+   title: "Watermelon juice",
+   price :62.00
+   },
+   {
+   id:3,
+   img: "https://hc.com.vn/i/ecommerce/media/ckeditor_3197317.jpg", 
+   title: "Pomegranate juice",
+   price :63.00
+   },
+   {
+   id:4,
+   img: "http://olivo.com.vn/wp-content/uploads/2021/03/nuoc-ep-tao-co-tac-dung-gi-2.png", 
+   title: "Apple juice",
+   price :59.00
+   },
+   {
+    id:5,
+    img: "https://truejuice.vn/articles/wp-content/uploads/2021/04/nuoc-ep-le-dao-thai-doc-to.jpg", 
+    title: "Pear juice",
+    price :67.00
+    },
+    {
+    id:6,
+    img: "https://blog.ptlvina.com/wp-content/uploads/2021/02/nuoc-ep-nho-2.jpg", 
+    title: "Grape juice",
+    price :55.00
+    },
+    {
+    id:7,
+    img: "https://quangon.vn/resources/2020/05/07/nuoc-ep-dau-tay-9.jpg", 
+    title: "Strawberry juice",
+    price :58.00
+    },   
+    {
+    id:8,
+    img: "https://truejuice.vn/articles/wp-content/uploads/2021/04/nuoc-ep-xoai.jpg", 
+    title: "Mangoes juice",
+    price :63.00
+    }, 
+    {
+    id:10,
+    img: "https://truejuice.vn/articles/wp-content/uploads/2021/06/nuoc-ep-dua-luoi-mix-dua-truejuice.jpg", 
+    title: "Melon juice",
+    price :64.00
+    },  
+    {
+    id:11,
+    img: "https://dammenaunuong.com/wp-content/uploads/2019/11/6-cong-dung-tuyet-voi-cua-nuoc-ep-dua.jpg", 
+    title: "Pineapple juice",
+    price :47.00
+    },
+    {
+    id:12,
+    img: "https://truejuice.vn/articles/wp-content/uploads/2021/06/nuoc-ep-chuoi-chin-truejuice-1536x1024.jpg", 
+    title: "Banana juice",
+    price :50.00
+    },
+    {
+    id:13,
+    img: "https://toplist.vn/images/800px/nuoc-ep-kiwi-725656.jpg", 
+    title: "Kiwi juice",
+    price :55.00  
+    },
+    {
+    id:14,
+    img: "https://win-rd.com/wp-content/uploads/2021/06/qua-mam-xoi-tac-dung-bat-ngo-va-luu-y-khi-dung-qu-m-m-x-i-v-l-x-th-m-1554796465-686-width600height397.jpg", 
+    title: "Raspberry juice",
+    price :60.00
+    },
+    {
+    id:15,
+    img: "https://toplist.vn/images/800px/nuoc-ep-nho-xanh-687395.jpg", 
+    title: "Green grapes juice",
+    price :59.00
+    },
+
+]
