8bc171a205ecface6959f4ad898eb2decf1cd107
.... <.>
2024-03-08 21:41:31 +0700

    Bill

--- my-react-app/src/Bill/Bill.jsx
+++ my-react-app/src/Bill/Bill.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import './Bill.scss'
 
-const BillPage = () => {
- 
-  return (
-    <div className='Bill'>
-      <h1>Bill Page</h1>
-      {/* Nội dung của trang hóa đơn */}
-    </div>
-  );
-};
+class BillPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      total: 0
+    };
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(name, price) {
+    const newItem = { name, price };
+    this.setState(prevState => ({
+      items: [...prevState.items, newItem],
+      total: prevState.total + price
+    }));
+  }
+
+  render() {
+    return (
+      <div className='Bill'>
+        <h1>Bill</h1>
+        <ul>
+          {this.state.items.map((item, index) => (
+            <li key={index}>{item.name}: ${item.price}</li>
+          ))}
+        </ul>
+        <p>Total: ${this.state.total}</p>
+        <button onClick={() => this.addItem('Item 1', 10)}>Add Item 1</button>
+        <button onClick={() => this.addItem('Item 2', 20)}>Add Item 2</button>
+        {/* Add more buttons for additional items */}
+      </div>
+    );
+  }
+}
 
 export default BillPage;
+
 
