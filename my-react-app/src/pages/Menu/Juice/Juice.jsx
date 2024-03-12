02a2c0220fd83a66e01e52d9d06d390a17962808
zukahm2005 <145833253+zukahm2005@users.noreply.github.com>
2024-03-12 16:24:43 +0700

    updateJuice

--- my-react-app/src/pages/Menu/Juice/Juice.jsx
+++ my-react-app/src/pages/Menu/Juice/Juice.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { dataProduct } from '../../../Data/DataHq'
 import {Link} from 'react-router-dom'
 import './Juice.scss'
 import { useShoppingContext } from '../../../Context/ShoppingContext'
 
 const Juice = () => {
+  const [activeLink, setActiveLink] = useState(null);
+
+  const handleLinkClick = (linkName) => {
+    setActiveLink(linkName);
+  };
   const { addCartItem } = useShoppingContext();
   return (
     <div className="juice-page">
@@ -14,10 +19,10 @@ const Juice = () => {
               </div>
         </div>
       <div className='juice-navbar'>
-        <div className='juice-link-to'><Link to="/VegetableJuice">Vegetable Juice</Link></div>
-        <div className='juice-link-to'><Link to="/Juice">Juice</Link></div>
-        <div className='juice-link-to'><Link to="/Protein">Protein</Link></div>
-        <div className='juice-link-to'><Link to="/Smoothies">Smoothies</Link></div>
+      <Link to="/VegetableJuice" className={activeLink === 'vegatablejuice' ? 'juice-navbar1 hover' : 'juice-navbar1'} onClick={() => handleLinkClick('vegatablejuice')}> Vegatable Juice</Link>
+        <Link to="/Juice" className={activeLink === 'juice' ? 'juice-navbar2 hover' : 'juice-navbar2'} onClick={() => handleLinkClick('juice')}>Juice</Link>
+        <Link to="/Protein" className={activeLink === 'protein' ? 'juice-navbar3 hover' : 'juice-navbar3'} onClick={() => handleLinkClick('protein')}>Protein</Link>
+        <Link to="/Smoothies" className={activeLink === 'smoothies ' ? 'juice-navbar4 hover' : 'juice-navbar4'} onClick={() => handleLinkClick('smoothies')}>Smoothies</Link>
       </div>
 
       
